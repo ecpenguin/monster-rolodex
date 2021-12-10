a876d6838a7a8fba147368fe83c57b279443d56d
Koji Takahashi <koji.takahashi@tech-corgi.com>
2021-12-10 10:37:50 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import {CardList} from "./components/card-list/card-list.component"
+import { SearchBox } from "./components/search-box/search-box.component";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render(){
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(mosnter =>
+      mosnter.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return(
+      <div className="App">
+        <h1>Monster Roldex</h1>
+        <SearchBox 
+          placeholder="search monsters" handleChange="" 
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>      
+    );
+  }
+
+}
+
+export default App;
